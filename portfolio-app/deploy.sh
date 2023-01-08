ac735e586986c629ef7d7874d933e3f175648d96
Tyler Blaszak <tab890@yahoo.com>
2023-01-08 15:13:03 -0500

    change refspec src

--- portfolio-app/deploy.sh
+++ portfolio-app/deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:TylerB890/TylerB890.github.io.git main:gh-pages
+git push -f git@github.com:TylerB890/TylerB890.github.io.git HEAD:main
 cd -

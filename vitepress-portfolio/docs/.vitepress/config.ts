8defd381a76754dfc8c7612cce22e91209d4c18a
Tyler Blaszak <tab890@yahoo.com>
2023-02-09 21:52:16 -0500

    change file structure to support logo and tab icon

--- vitepress-portfolio/docs/.vitepress/config.ts
+++ vitepress-portfolio/docs/.vitepress/config.ts
@@ -41,7 +41,7 @@ export default defineConfig({
   head: [
     [
       "link",
-      { rel: "icon", type: "image/svg+xml", href: "/assets/logo-black.ico" },
+      { rel: "icon", type: "image/png", href: "/assets/logo-black.png" },
     ],
   ],
 });
